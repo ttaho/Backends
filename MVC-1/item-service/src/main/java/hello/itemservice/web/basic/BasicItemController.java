fc88f3b96d52c2e5da7aa274f55e0bb6694d7147
Taeho <ice_98@naver.com>
2023-05-08 00:55:20 +0900

    #11 - 상품 목록 - 타임리프

---
+++ MVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,42 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor //final이 붙은거의 생성자를 자동으로 생성해줌. 그래서 아래 주석처리함.
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired //itemRepository에 맞는것을 자동으로 의존관계 주입해줌
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "basic/items";
+    }
+
+    /**
+     * 태스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 1000, 10));
+        itemRepository.save(new Item("itemB", 2000, 10));
+    }
+}
