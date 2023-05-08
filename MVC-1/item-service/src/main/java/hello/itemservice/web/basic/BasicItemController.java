81d95eab5bc6da0fb8446bc6fc9f5d1a3e990dc1
Taeho <ice_98@naver.com>
2023-05-09 00:40:05 +0900

    #11 - 상품 수정

--- MVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ MVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -67,6 +67,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId,
+                            @ModelAttribute("item") Item item) {
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
     /**
      * 태스트용 데이터 추가
      */
