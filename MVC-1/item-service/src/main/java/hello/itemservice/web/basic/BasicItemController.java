dcd244328482e3c57e286c8ba11c176818714206
Taeho <ice_98@naver.com>
2023-05-09 01:10:26 +0900

    #11 - RedirectAttributes

--- MVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ MVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -67,13 +68,21 @@ public class BasicItemController {
         return "basic/item";
     }
     //PRG 적용
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute("item") Item item) {
 
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
     }
-    
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute("item") Item item,
+                            RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
